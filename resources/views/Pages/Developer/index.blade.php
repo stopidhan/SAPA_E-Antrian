4dea155d5024fdb54cb7afd26f50e19438529fb1
stopidhan <144976882+stopidhan@users.noreply.github.com>
2026-07-15 11:58:52 +0700

    minor changes

--- resources/views/Pages/Developer/index.blade.php
+++ resources/views/Pages/Developer/index.blade.php
@@ -46,6 +46,9 @@
                                 Slug / Domain</th>
                             <th scope="col"
                                 class="px-6 py-4 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">
+                                Nomor Whatsapp</th>
+                            <th scope="col"
+                                class="px-6 py-4 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">
                                 Status</th>
                             <th scope="col"
                                 class="px-6 py-4 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">
@@ -81,6 +84,17 @@
                                             </path>
                                         </svg>
                                         {{ $instance->instance_slug }}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
+                                    <div class="flex items-center">
+                                        <svg class="w-4 h-4 text-gray-400 mr-1.5" fill="none" stroke="currentColor"
+                                            viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
+                                            </path>
+                                        </svg>
+                                        {{ $instance->whatsapp_number }}
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
